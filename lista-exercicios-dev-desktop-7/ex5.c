e0a64ff133971a82c45d70a0e8e16510abf013c4
Matheus <mf5881585@gmail.com>
2024-05-03 21:51:49 -0300

    ex5

--- lista-exercicios-dev-desktop-7/ex5.c
+++ lista-exercicios-dev-desktop-7/ex5.c
@@ -1,2 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+void converterParaOutrasMedidas(float metros) {
+    float decimetros, centimetros, milimetros;
+
+
+    decimetros = metros * 10;
+    centimetros = metros * 100;
+    milimetros = metros * 1000;
+
+
+    printf("%.2f metros correspondem a:\n", metros);
+    printf("%.2f decimetros\n", decimetros);
+    printf("%.2f centimetros\n", centimetros);
+    printf("%.2f milimetros\n", milimetros);
+}
+
+int main () {
+    float metros;
+
+    printf("Digite um valor em metros: ");
+    scanf("%f", &metros);
+
+    converterParaOutrasMedidas(metros);
+}
